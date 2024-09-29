5ef17724956df1eb6e199d35c4e3aca663a0426c
RahulVishvakama <rv.rahulvan1311@gmail.com>
2024-09-29 18:14:53 +0000

    variable declaration done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234321
+let accountEmail = "rh@gmail.com" // variable declaration
+// var acountPassword = "123456" now a days we do not use var for variable declaration because it has scope problem
+// accountCity = "ahmedabad"
+
+// accountId = 3
+
+// console.log(accountId) not allowed 
+
+accountEmail = "rv.rahulvan@gmail.com"
+accountPassword = "456775"
+accountCity = "rajkot"
+
+console.table([accountId , accountEmail , accountPassword, accountCity])
